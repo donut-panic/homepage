5bc2ea7fbe4efe0ad7d96e21513fbc2520c26ea5
Jarek Karczewski <yaroslawek@gmail.com>
2019-10-12 13:10:41 +0200

    Revert "Add webpack"
    This reverts commit e4c2064aa5897fa237210da7ed40f60765a771ed.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+// Preloader
+const body = document.querySelector('body');
+const preloader = document.querySelector('.preloader');
+const animatedElements = document.querySelectorAll('[data-animate]');
+
+window.addEventListener('load', () => {
+    preloader.addEventListener('transitionend', function () {
+        body.classList.add('body--loaded');
+        this.classList.add('preloader--hidden');
+        animatedElements.forEach(element => {
+            element.classList.add(element.classList[0] + '--hasLoaded');
+        });
+    });
+    preloader.classList.add('preloader--disappear');
+});
+
+// Scroll to top
+const scrollToTopTriggers = document.querySelectorAll('.scroll-to-top');
+
+scrollToTopTriggers.forEach(trigger => {
+    trigger.addEventListener('click', e => {
+        e.preventDefault();
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
+});
+
 // Contact form
 const form = document.querySelector('form');
 const email = document.querySelector('[name="email"]');
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-// JS files
-// import 'preloader.js';
-// import 'scroller.js';
-
-// CSS files
-import '../scss/main.scss';
